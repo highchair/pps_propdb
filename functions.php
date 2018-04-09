a46bf45e1981b57cb00b94c09f70cce47ad6f6de
kangabell <k@kangabell.co>
2018-04-09 19:03:05 -0400

    add google maps API key for ACF admin stuff

--- functions.php
+++ functions.php
@@ -28,4 +28,21 @@ function ppsdb_scripts_and_styles() {
 
 add_action('wp_enqueue_scripts', 'ppsdb_scripts_and_styles', 999);
 
+
+/*********************
+FUNCTIONS
+*********************/
+
+function ppsdb_acf_google_map_api( $api ){
+
+    $api['key'] = 'AIzaSyDIqb19OYrLxZvikR_yVnPTDnhsCsP0vtA';
+
+    return $api;
+
+}
+
+add_filter('acf/fields/google_map/api', 'ppsdb_acf_google_map_api');
+
+
+
 ?>
