d2f965bcad5f91add0a306c785e59832fd99601c
kangabell <k@kangabell.co>
2018-04-15 15:42:21 -0400

    create Properties archive

---
+++ archive-property.php
@@ -0,0 +1,38 @@
+<!--
+PROPERTIES ARCHIVE PAGE TEMPLATE
+-->
+
+<?php get_header(); ?>
+
+<main>
+  
+  <nav class="breadcrumb">
+    <?php bcn_display(); ?>
+  </nav>
+
+  <div class="main">
+
+    <header>
+
+      <h1>
+        <?php
+          _e('All Properties', 'ppsdb');
+        ?>
+      </h1>
+
+    </header>
+    
+    <?php
+    if ( have_posts() ) { 
+      while ( have_posts() ) { the_post();
+        get_template_part('partials/property-sm');
+      }
+      get_template_part('partials/pagination');
+    }
+    ?>
+
+  </div>
+
+</main>
+
+<?php get_footer(); ?>
