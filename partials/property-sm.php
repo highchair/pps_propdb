35b48e4e1f8f523347a66268629db1a8504f6f87
kangabell <k@kangabell.co>
2018-08-15 13:07:14 -0400

    style related properties on single Tour

--- partials/property-sm.php
+++ partials/property-sm.php
@@ -7,11 +7,14 @@ $location = get_field('location');
 
     <?php the_post_thumbnail('grid-thumb'); ?>
 
-    <h3><?php the_title(); ?></h3>
+    <div class="heading">
+      <h3><?php the_title(); ?></h3>
+      <?php
+      if ( get_field('aka') ) { echo '<p class="aka">&#123; ' . get_field('aka') . ' &#125;</p>'; }
+      ?>
+    </div>
 
     <?php
-    if ( get_field('aka') ) { echo '<p>&#123; ' . get_field('aka') . ' &#125;</p>'; }
-
     echo '<p>' . $location['address'] . '</p>';
 
     if ( get_field('period') ) { echo '<p>' . get_field('period') . '</p>'; }
