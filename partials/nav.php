c7c8f878697b4e6134c60ca85c0c931a98d23405
kangabell <k@kangabell.co>
2018-08-22 16:16:36 -0400

    move link to main site into header secondary nav area; add more logo text

--- partials/nav.php
+++ partials/nav.php
@@ -1,21 +1,25 @@
 <header id="header">
 	<a href="<?php echo get_home_url(); ?>">
-		<h1 class="logo-text">Providence Preservation Society</h1>
+		<h1 class="logo-text">
+      Providence Preservation Society<br />
+      Providence Property Database
+    </h1>
 	</a>
 	<nav role="navigation">
 
 		<div class="secondary-nav">
 			<a class="toggle-nav" href="javascript:void(0)"><span class="icon-bars"></span> Menu</a> 
-			<div class="desktop-search"><a href="javascript:void(0)">Search</a></div>
+			<div class="desktop-search">
+        <a href="javascript:void(0)">Search</a>
+        <a href="<?php echo network_home_url(); ?>"><?php _e( 'PPSri.org', 'ppsdb' ); ?> &rarr;</a>
+      </div>
 		</div>
 
 		<div class="primary-nav">
 			<a class="toggle-nav" href="javascript:void(0)"><span class="icon-close"></span> Exit</a>
-		  	<?php ppsri_primary_nav(); ?>
+      <?php ppsri_primary_nav(); ?>
 			<?php echo get_search_form(); ?>
 		</div>
-
-		<a href="<?php echo network_home_url(); ?>"><?php _e( 'PPSri.org', 'ppsdb' ); ?> &rarr;</a>
 
 	</nav>
 
