e3544dca77159c3a9bc36f825efcbc1ace4b0a8e
kangabell <k@kangabell.co>
2019-04-04 16:06:38 -0400

    turn taxonomy filters into a dropdown menu

--- partials/taxonomies-browse.php
+++ partials/taxonomies-browse.php
@@ -4,56 +4,50 @@
     <?php _e('Browse by Category', 'ppsdb'); ?>
   </h2>
 
-  <div>
+  <ul class="dropdown menu" data-dropdown-menu>
 
-    <div>
-      <a href="#"><h3><?php _e('Architectural Styles', 'ppsdb'); ?></h3></a>
-      <div>
-        <ul>
-          <?php
-          $styles = get_terms('architectural_style');
-          foreach ($styles as $style):
-          ?>
-            <li><a href="<?php echo get_term_link($style); ?>"><?php echo $style->name; ?></a></li>
-          <?php
-          endforeach;
-          ?>
-        </ul>
-      </div>
-    </div>
+    <li>
+      <a href="#"><?php _e('Architectural Styles', 'ppsdb'); ?></a>
+      <ul class="menu">
+        <?php
+        $styles = get_terms('architectural_style');
+        foreach ($styles as $style):
+        ?>
+          <li><a href="<?php echo get_term_link($style); ?>"><?php echo $style->name; ?></a></li>
+        <?php
+        endforeach;
+        ?>
+      </ul>
+    </li>
 
-    <div>
-      <a href="#"><h3><?php _e('Designers', 'ppsdb'); ?></h3></a>
-      <div>
-        <ul>
-          <?php
-          $designers = get_terms('designer');
-          foreach ($designers as $designer):
-          ?>
-            <li><a href="<?php echo get_term_link($designer); ?>"><?php echo $designer->name; ?></a></li>
-          <?php
-          endforeach;
-          ?>
-        </ul>
-      </div>
-    </div>
+    <li>
+      <a href="#"><?php _e('Designers', 'ppsdb'); ?></a>
+      <ul class="menu">
+        <?php
+        $designers = get_terms('designer');
+        foreach ($designers as $designer):
+        ?>
+          <li><a href="<?php echo get_term_link($designer); ?>"><?php echo $designer->name; ?></a></li>
+        <?php
+        endforeach;
+        ?>
+      </ul>
+    </li>
 
-    <div>
-      <a href="#"><h3><?php _e('Neighborhoods', 'ppsdb'); ?></h3></a>
-      <div>
-        <ul>
-          <?php
-          $neighborhoods = get_terms('neighborhood');
-          foreach ($neighborhoods as $neighborhood):
-          ?>
-            <li><a href="<?php echo get_term_link($neighborhood); ?>"><?php echo $neighborhood->name; ?></a></li>
-          <?php
-          endforeach;
-          ?>
-        </ul>
-      </div>
-    </div>
+    <li>
+      <a href="#"><?php _e('Neighborhoods', 'ppsdb'); ?></a>
+      <ul class="menu">
+        <?php
+        $neighborhoods = get_terms('neighborhood');
+        foreach ($neighborhoods as $neighborhood):
+        ?>
+          <li><a href="<?php echo get_term_link($neighborhood); ?>"><?php echo $neighborhood->name; ?></a></li>
+        <?php
+        endforeach;
+        ?>
+      </ul>
+    </li>
 
-  </div>
+  </ul>
 
 </aside>
