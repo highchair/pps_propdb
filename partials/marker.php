919f3ee1008fa4eee8b1b342e0c9c6aced599247
kangabell <k@kangabell.co>
2019-04-10 16:05:34 -0400

    add aka name & address to map infowindow

--- partials/marker.php
+++ partials/marker.php
@@ -4,6 +4,14 @@ GOOGLE MAP MARKER & INFOWINDOW
 
 <div class="marker" data-lat="<?php echo $location['lat']; ?>" data-lng="<?php echo $location['lng']; ?>">
 	<a href="<?php echo esc_url( add_query_arg( 't', $tour, get_permalink() ) );?>">
-		<?php echo get_the_title($id); ?>
+		<?php
+		echo '<h4>' . get_the_title($id) . '</h4>';
+		if ( get_field('aka') ) {
+			echo '<p class="aka">&#123; ' . get_field('aka') . ' &#125;</p>';
+		}
+		if ( isset($location['address']) ) {
+			echo '<p class="address">' . $location['address'] . '</p>';
+		}
+		?>
 	</a>
 </div>
